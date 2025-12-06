1f88b85fa01d7c31518eebf5f0fc17e143db3eb5
a. Elkhalily <a.elkhalily10@gmail.com>
2025-12-06 22:02:06 +0200

    [FORM] todo list

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,56 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Hello React</div>;
+  const [task, setTask] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [isTaskValid, setIsTaskValid] = useState(true);
+
+  function addTaskToTodoList(event) {
+    event.preventDefault();
+    if (!task) {
+      setIsTaskValid(false);
+      return;
+    }
+
+    setIsTaskValid(true);
+    setTodoList((prev) => {
+      return [...prev, task];
+    });
+
+    setTask("");
+  }
+
+  return (
+    <div>
+      <h1 className="heading">My First TODO</h1>
+      <form className="form">
+        <div className="form-control">
+          <label>Task Name</label>
+          <input
+            className={`input ${!isTaskValid ? "error" : ""}`}
+            value={task}
+            onChange={(event) => {
+              if (task) setIsTaskValid(true);
+              setTask(event.target.value);
+            }}
+            placeholder="please add your task "
+          />
+          {!isTaskValid ? <small>Task is required</small> : null}
+        </div>
+        <button onClick={addTaskToTodoList}>Add Task</button>
+      </form>
+      <hr></hr>
+      <div>
+        <h4>Task Items</h4>
+        <ul>
+          {todoList.map((task, index) => {
+            return <li key={index}>{task}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
