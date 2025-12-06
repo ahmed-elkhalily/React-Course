3c930b9ca63e486b79c6853d7bbfc346a7f0adda
a. Elkhalily <a.elkhalily10@gmail.com>
2025-12-06 19:25:46 +0200

    Init

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,52 +1,7 @@
 import "./App.css";
 
 function App() {
-  const todoLIst = [
-    {
-      title: "Finish My Homework",
-      progress: 100,
-    },
-    {
-      title: "Developing New Web App",
-      progress: 52,
-    },
-    {
-      title: "Developing Ecommerce App",
-      progress: 25,
-    },
-    {
-      title: "Cleaning My Bedroom",
-      progress: 0,
-    },
-  ];
-
-  function generateProgressClassName(progress) {
-    return progress === 100 ? "completed" : progress > 0 ? "onwork" : "not-yet";
-  }
-
-  return (
-    <ul>
-      {todoLIst.map((listItem, index) => {
-        return (
-          <li
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              fontSize: 50,
-            }}
-            key={index}
-          >
-            <span className="title">{listItem.title.toUpperCase()}</span>
-            <span
-              className={`progress ${generateProgressClassName(
-                listItem.progress
-              )}`}
-            >{`${listItem.progress}%`}</span>
-          </li>
-        );
-      })}
-    </ul>
-  );
+  return <div>Hello React</div>;
 }
 
 export default App;
